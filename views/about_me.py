19b32c32aa0146067c163444a95f636a439028a2
freetimezp <kraftukrnet@ukr.net>
2024-08-18 20:12:54 +0300

    04. py-02-multipage. About Me page.

--- views/about_me.py
+++ views/about_me.py
@@ -1,3 +1,57 @@
 import streamlit as st
 
-st.title("About Me")
+@st.dialog("Contact Me")
+def show_contact_form():
+    st.text_input(label= "First Name", placeholder="First Name")
+
+# --- hero section ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+
+with col1:
+    st.image("./assets/images/avatar.jpg", width=230)
+
+with col2:
+    st.title("Web Developer", anchor=False)
+    st.write(
+        "Frontend & Backend Developer from Ukraine, simply dummy text of the printing and typesetting industry."
+    )
+    if st.button("Contact Me"):
+        show_contact_form()
+
+
+# --- experience ---
+st.write("\n")
+st.subheader("Experience & Qualifications", anchor=False)
+st.write(
+    """
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    """
+)
+
+# --- skills ---
+st.write("\n")
+st.subheader("My Skills", anchor=False)
+st.write(
+    """
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    - simply dummy text of the printing and typesetting industry
+    """
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
