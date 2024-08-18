bd704fdfa41aaaa8693d12d1432ab45194876278
freetimezp <kraftukrnet@ukr.net>
2024-08-18 19:15:33 +0300

    02. py-02-multipage. Menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,41 @@
-start
+import streamlit as st
+
+# --- page setup ---
+
+about_page = st.Page(
+    page="views/about_me.py",
+    title="About Me",
+    icon=":material/account_circle:",
+    default=True,
+)
+
+project_1_page = st.Page(
+    page="views/sales_dashboard.py",
+    title="Sales Dashboard",
+    icon=":material/bar_chart:",
+)
+
+project_2_page = st.Page(
+    page="views/chatbot.py",
+    title="Chatbot",
+    icon=":material/smart_toy:",
+)
+
+
+# --- navigation setup [without sections]---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- navigation setup [with sections]---
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page],
+    }
+)
+
+
+# --- run navigation ---
+
+pg.run()
+
+
