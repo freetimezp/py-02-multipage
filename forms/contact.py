16b02d1530a21e3c1ce848d72537b8b2aa9a7063
freetimezp <kraftukrnet@ukr.net>
2024-08-18 21:41:38 +0300

    06. py-02-multipage. About Me page. Contact form. Validation. Sent request to Pabbly.

--- forms/contact.py
+++ forms/contact.py
@@ -1,4 +1,15 @@
+import re
 import streamlit as st
+import requests
+
+# --- webhook from pabbly ---
+WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjUwNTY0MDYzMTA0MzA1MjY1NTUzMjUxM2Ii_pc"
+
+# --- form validation ---
+def is_valid_email(email):
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(email_pattern, email) is not None
+
 
 def contact_form():
     with st.form("contact_form"):
@@ -8,8 +19,31 @@ def contact_form():
         submit_button = st.form_submit_button("Submit")
 
         if submit_button:
-            st.success("Message successfully sent..")
+            if not WEBHOOK_URL:
+                st.error("Email service is not set up. Please try again later..")
+                st.stop()
+            
+            if not name:
+                st.error("Please provide your name..")
+                st.stop()
+            if not email:
+                st.error("Please provide your email..")
+                st.stop()
+            if not is_valid_email(email):
+                st.error("Please provide a valid email..")
+                st.stop()
+            if not message:
+                st.error("Please provide message..")
+                st.stop()
 
+            # --- prepare data payload and send it to the webhook url ---
+            data = {"email": email, "name": name, "message": message}
+            response = requests.post(WEBHOOK_URL, json=data)
+
+            if response.status_code == 200:
+                st.success("Your message has sent successfully..")
+            else:
+                st.error("Error while sending message..")
 
 
 
